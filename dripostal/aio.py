cd312d81eae16e5e09a96624cb1501da1e2f1cbb
cerberus <sidica@protonmail.com>
2022-06-10 21:11:40 -0400

    modified for mass requests

--- dripostal/aio.py
+++ dripostal/aio.py
@@ -17,9 +17,28 @@ except ModuleNotFoundError as e:
 from . import DriPostal as _BaseDriPostal
 from .schemas import Address
 
+from pydantic import AnyHttpUrl
+
+# modified so that clientsession is passed into the class
 
 class DriPostal(_BaseDriPostal):
     """Asynchronous version of the DriPostal client."""
+    
+    def __init__(
+        self, url: str, session, parse_method: str = "parse", expand_method: str = "expand"
+    ):
+        """Dripostal configuration.
+
+        Args:
+            url: URL of the Libpostal service.
+            parse_method: Parse method name in the API.
+            expand_method: Expand method name in the API.
+
+        """
+        self.service_url: AnyHttpUrl = self._parse_url(url)
+        self._parse_method: str = parse_method
+        self._expand_method: str = expand_method
+        self.session = session
 
     async def parse(self, address: str) -> Address:  # type: ignore
         """Parse an address with Libpostal.
@@ -31,10 +50,10 @@ class DriPostal(_BaseDriPostal):
 
         """
         request_url = self._get_url(method="parse", address=address)
-        async with ClientSession() as session:
-            async with session.get(request_url) as response:
-                payload = await response.json()
-                return Address(**{el["label"]: el["value"] for el in payload})
+
+        async with self.session.get(request_url) as response:
+            payload = await response.json()
+            return Address(**{el["label"]: el["value"] for el in payload})
 
     async def expand(self, address: str) -> List[str]:  # type: ignore
         """Expand an address with Libpostal.
@@ -46,9 +65,10 @@ class DriPostal(_BaseDriPostal):
 
         """
         request_url = self._get_url(method="expand", address=address)
-        async with ClientSession() as session:
-            async with session.get(request_url) as response:
-                return await response.json()
+
+        async with self.session.get(request_url) as response:
+            return await response.json()
 
 
 __all__ = ["DriPostal"]
+
